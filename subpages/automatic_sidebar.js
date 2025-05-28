21d3f69873f12a9cde2423988e754a6bea7b9477
milena-t <milena.trabert@t-online.de>
2025-05-28 21:24:29 +0200

    setup with automatic JS sidebar and basic css

---
+++ subpages/automatic_sidebar.js
@@ -0,0 +1,44 @@
+function generateRandomString(length) {
+    return Math.random().toString(36).substring(2, 2 + length);
+}
+/**
+ * @param {HTMLElement} el
+ */
+function make_sidebar(el){
+    /** 
+     * @type {{title:string , subsections:{ title:string , node:HTMLElement}[] , node:HTMLElement}[]}
+     */
+    let projects = [];
+    for (let child of el.children){
+        if (child.tagName.toLowerCase() == "h1"){
+            projects.push({
+                title: child.textContent,
+                subsections : [],
+                node:child
+            });
+        }
+        if (child.tagName.toLowerCase() == "h2"){
+            projects[projects.length-1].subsections.push({
+                title: child.textContent,
+                node: child
+            })
+        }
+    }
+    for (let p of projects){
+        let element_id = p.node.getAttribute("id")
+        if (element_id == null){
+            element_id = generateRandomString(10)
+            p.node.setAttribute("id", element_id)
+        }
+
+        let projectname = document.createElement("a")
+        projectname.innerText = p.title
+        projectname.setAttribute("href", "#"+element_id)
+        projectname.classList.add("project-link")
+        let sidebar = document.getElementById("sidebar-contents")
+        sidebar.appendChild(projectname)
+    }
+}
+window.addEventListener("load", ()=>{
+make_sidebar(document.getElementById("main-body"))
+})
