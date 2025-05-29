7fda40d94e8befe862425936d63a3f2af33a7c73
milena-t <milena.trabert@t-online.de>
2025-05-29 16:55:10 +0200

    remove old heic

--- compress_heic.sh
+++ compress_heic.sh
@@ -4,5 +4,5 @@ input="$1"
 basename="${input%.*}"
 jpeg="${basename}.jpeg"
 sips -s format jpeg "$input" --out "$jpeg"
-
+rm $input
 exiftool -all= -overwrite_original $jpeg && jpegoptim --strip-all --max=85 $jpeg
