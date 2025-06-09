6be2189145e5848884ab7f494a2774e1976d407f
milena-t <milena.trabert@t-online.de>
2025-06-09 21:10:09 +0200

    change max compression to lower

--- compress_heic.sh
+++ compress_heic.sh
@@ -5,4 +5,4 @@ basename="${input%.*}"
 jpeg="${basename}.jpeg"
 sips -s format jpeg "$input" --out "$jpeg"
 rm $input
-exiftool -all= -overwrite_original $jpeg && jpegoptim --strip-all --max=85 $jpeg
+exiftool -all= -overwrite_original $jpeg && jpegoptim --strip-all --max=75 $jpeg
